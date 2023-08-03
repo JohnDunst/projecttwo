dc7e9e555500512b71f4bb88a9f87257822ae490
Jonathan Dunst <johnnydunst6@outlook.com>
2023-08-03 07:06:31 -0400

    css header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
+import './style.css'
 
 const App = () => {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 
 
+
+
 const Header = () => {
   return (
-    <header>
-
+ <div className= "navContainer">
       <h1>Joanthan Dunst</h1>
       <p>Software Engineer | idk</p>
-    
-<nav>
-<ul>
-  <li>
+
+  <div className='scrollContainer'>
+
     <div className="#about">About  </div>
-  </li>
-  <li>
+
+
     <div className="#projects">Projects</div>
-  </li>
-  <li>
+
     <div className="#contact">Contact</div>
-  </li>
- 
-</ul>
-</nav>
-</header>
+
+ </div>
+
+</div>
+
+
+
 
   );
 };
