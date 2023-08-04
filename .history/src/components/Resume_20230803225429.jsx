dea8a270286312dd7a62fd2995ecd053baf31a6b
Jonathan Dunst <johnnydunst6@outlook.com>
2023-08-03 23:12:31 -0400

    ddsd

---
+++ .history/src/components/Resume_20230803225429.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Resume = () => {
+return (
+    <div className="Resume">
+ {/* <button> Resumer </button>  */}
+
+    </div>
+);
+
+
+
+}
+
+export default Resume;zz
+export default Resume;zz
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React from "react";
+import CV from "../assets/Dunst Resume.pdf";
+import Resume from "./Resume";
 
 const Header = () => {
+
+
+
+
   const scrollToAbout = () => {
-    const aboutSection = document.getElementById('about');
+    const aboutSection = document.getElementById("about");
     if (aboutSection) {
-      aboutSection.scrollIntoView({ behavior: 'smooth' });
+      aboutSection.scrollIntoView({ behavior: "smooth" });
     }
   };
 
+
+
+  const openResume = () => {
+    window.open(CV, "Dunst Resume.pdf"); // Open the PDF in a new browser tab
+  };
+
+
+
+
+
   return (
     <div>
-      <div className='headerConatiner'>
-      <h1>Jonathan Dunst</h1>
-      
-   
-      <p>Software Engineer | Frontend Developer</p>
-  
-        
-        <ul className='linkContainer'>
-        
+      <div className="headerConatiner">
+        <ul className="linkContainer">
+          <a href="spicy" className="nav__logo">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTsOxv6UAiGwL2tSXdFUM0rM63enIOaDz2eQ&usqp=CAU"
+              alt="Nevergiveup"
+            />
+          </a>
           <li>
             <a href="#about" onClick={scrollToAbout}>
               About
@@ -31,12 +46,13 @@ const Header = () => {
             <a href="#contact">Contact</a>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a href="#resume" onClick={openResume}>
+              resume
+            </a>
           </li>
         </ul>
-        </div>
+      </div>
     </div>
   );
 };
-
 export default Header;
